26d284c6e8b1280eab2320966c4618d8584656f6
Johnny Hu <jgeehen.hu@gmail.com>
2017-03-31 05:43:08 +0000

    Refactor code

--- drunken.js
+++ drunken.js
@@ -7,14 +7,11 @@ var data = {
 var resetButton = document.querySelector("#resetButton");
 var easyButton = document.querySelector("#easyButton");
 var hardButton = document.querySelector("#hardButton");
-var difficulty = "easy";
 var gameOver = false;
-var boardSize;
-var gridSize;
+var gridCount = 7;
 
 // Initialization
-init();
-
+newGame();
 
 /**
  ** EVENT LISTENERS
@@ -41,35 +38,30 @@ document.body.addEventListener("keydown", function(e){
         if(currentPosition.classList.contains("end")){
             gameOver = true;
             var marqueeMessage = document.querySelector(".marquee__message");
-            
             marqueeMessage.textContent = "Congratulations! You made it!";
             resetButton.textContent = "Play again?";
         }
     }
 });
     
-resetButton.addEventListener("click", function(){
-    resetBoard();
-    init();
-});
+resetButton.addEventListener("click", newGame);
 
 hardButton.addEventListener("click", function(){
-    difficulty = "hard";
-    resetBoard();
-    init();
+    gridCount = 10;
+    newGame();
 });
 
 easyButton.addEventListener("click", function(){
-    difficulty = "easy";
-    resetBoard();   
-    init();
+    gridCount = 7;
+    newGame();
 });
 
 /**
  ** FUNCTIONS
  **/
 
-function init(){
+function newGame(){
+    resetBoard();
     createBoard();
     setupBoard();
     generateStartAndEnd();
@@ -85,31 +77,23 @@ function createBoard(){
 function setupBoard(){
     var board = document.querySelector("#board");
     var count = 1;
+    var squareSize = 700 / gridCount;
     
-    if(difficulty === "easy"){
-        boardSize = 50;
-    } else if(difficulty === "hard"){
-        boardSize = 100;
-    }
-    gridSize = Math.floor(Math.sqrt(boardSize));    
-    
-    for(var x = 1; x <= gridSize; x++){
-        for(var y = 1; y <= gridSize; y++){
-            var squareSize = 500 / gridSize;
+    for(var x = 1; x <= gridCount; x++){
+        for(var y = 1; y <= gridCount; y++){
             var newSquare = document.createElement("div");
             newSquare.className = "square row" + x + " col" + y;
             newSquare.id = count;
-            newSquare.style.width = squareSize + "px";
-            newSquare.style.height = squareSize + "px";
+            newSquare.setAttribute("style", "width: " + squareSize + "px; height: " + squareSize + "px;");
             
             // Assign HTML data attributes identifying whether a square is at the edge of the board.
             x === 1 ? newSquare.setAttribute("top-edge", true) : newSquare.setAttribute("top-edge", false);
             // True if square is on left edge of board.
             y === 1 ? newSquare.setAttribute("left-edge", true) : newSquare.setAttribute("left-edge", false);
             // True if square is on bottom edge of board.
-            x === gridSize ? newSquare.setAttribute("bottom-edge", true) : newSquare.setAttribute("bottom-edge", false);
+            x === gridCount ? newSquare.setAttribute("bottom-edge", true) : newSquare.setAttribute("bottom-edge", false);
             // True if square is on right edge of board.
-            y === gridSize ? newSquare.setAttribute("right-edge", true) : newSquare.setAttribute("right-edge", false);
+            y === gridCount ? newSquare.setAttribute("right-edge", true) : newSquare.setAttribute("right-edge", false);
             
             board.append(newSquare);
             count++;
@@ -127,11 +111,13 @@ function resetBoard(){
     if(marqueeMessage.textContent !== "") marqueeMessage.textContent = "";        
     
     // Remove and recreate board
-    document.querySelector("#board").remove();
+    var board = document.querySelector("#board");
+    if(board) board.remove();
 }
 
 function generateStartAndEnd() {
     var start, end;
+    var boardSize = gridCount * gridCount;
     while(start === end) {
         // Randomly selects a startying square and goal
         start = Math.floor(Math.random() * boardSize + 1);
@@ -145,9 +131,9 @@ function generateStartAndEnd() {
 // Move to next position on board based on current position
 function moveToNextPosition(current, keypressed){
     if(keypressed === "ArrowUp"){
-        return document.getElementById(Number(current.id) - gridSize);    
+        return document.getElementById(Number(current.id) - gridCount);    
     } else if(keypressed === "ArrowDown"){
-        return document.getElementById(Number(current.id) + gridSize);    
+        return document.getElementById(Number(current.id) + gridCount);    
     } else if(keypressed === "ArrowLeft"){
         return document.getElementById(Number(current.id) - 1);    
     } else {
